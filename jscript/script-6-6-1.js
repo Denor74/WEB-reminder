8bd9302eba9b53288c808872a2aef3d1fa03cbca
Denor74 <den74@inbox.ru>
2022-12-10 21:12:17 +0500

    script6-6-1 error

--- jscript/script-6-6-1.js
+++ jscript/script-6-6-1.js
@@ -1,4 +1,4 @@
-const userText = prompt('Введите текст');
-const userTextField = document.querySelector('#userTextField');
-userTextField.textContent = userText;
-console.log('Текст в блоке с id userTextField изменён на', userText);
+const userTextIDFild = prompt('Введите текст');
+const userTextIDField = document.querySelector('#userTextIDField');
+userTextIDField.textContent = userTextID;
+console.log('Текст в блоке с id userTextField изменён на', userTextID);
